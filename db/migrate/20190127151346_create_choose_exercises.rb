9e2cb7ebbbf1504f6d5486177564a4d902ee4c17
Ivanov Artem <ivanovartemwork94@gmail.com>
2019-03-09 23:44:16 +0400

    Update db schema

--- db/migrate/20190127151346_create_choose_exercises.rb
+++ db/migrate/20190127151346_create_choose_exercises.rb
@@ -2,7 +2,7 @@ class CreateChooseExercises < ActiveRecord::Migration[5.2]
   def change
     create_table :choose_exercises do |t|
       t.string :variants, null: false, array: true
-      t.integer :answer_index, null: false
+      t.integer :answer, null: false
 
       t.timestamps
     end
--- db/migrate/20190303092838_rename_answer_index_column_of_choose_exercise.rb
+++
@@ -1,5 +0,0 @@
-class RenameAnswerIndexColumnOfChooseExercise < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :choose_exercises, :answer_index, :answer
-  end
-end
